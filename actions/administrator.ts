c6bde03c2ef3633c5dd7efe60c01337125309d10
IRTIDEATH <enderjhass@gmail.com>
2025-04-12 10:28:11 +0700

    Membuat server action administrator (Get)

---
+++ actions/administrator.ts
@@ -0,0 +1,74 @@
+"use server"
+
+import prisma from "@/lib/prisma"
+import { QueriesResponse } from "@/lib/response/administrator"
+import { KehadiranEnum } from "@prisma/client"
+
+
+export async function getTotalAbsenKehadiran(): Promise<QueriesResponse | undefined> {
+    try {
+        const today = new Date()
+        const startOfDay = new Date(today.setHours(0, 0, 0, 0))
+        const endOfDay = new Date(today.setHours(23, 59, 59, 999))
+
+        const [hadir, terlambat, izin, alpha, sakit] = await Promise.all([
+            prisma.kehadiran.count({
+                where: {
+                    kehadiran: 'hadir',
+                    tanggal: {
+                        gte: startOfDay,
+                        lte: endOfDay
+                    }
+                }
+            }),
+            prisma.kehadiran.count({
+                where: {
+                    kehadiran: 'terlambat',
+                    tanggal: {
+                        gte: startOfDay,
+                        lte: endOfDay
+                    }
+                }
+            }),
+            prisma.kehadiran.count({
+                where: {
+                    kehadiran: 'izin',
+                    tanggal: {
+                        gte: startOfDay,
+                        lte: endOfDay
+                    }
+                }
+            }),
+            prisma.kehadiran.count({
+                where: {
+                    kehadiran: 'alpha',
+                    tanggal: {
+                        gte: startOfDay,
+                        lte: endOfDay
+                    }
+                }
+            }),
+            prisma.kehadiran.count({
+                where: {
+                    kehadiran: 'sakit',
+                    tanggal: {
+                        gte: startOfDay,
+                        lte: endOfDay
+                    }
+                }
+            })
+        ])
+
+        return {
+            data: {
+                totalHadir: hadir,
+                totalTerlambat: terlambat,
+                totalIzin: izin,
+                totalAlpha: alpha,
+                totalSakit: sakit
+            }
+        }
+    } catch (error) {
+        console.error('Error fetching total absen:', error)
+    }
+}
