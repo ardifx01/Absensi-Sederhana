35143cc9bfb4369bdc4d93c0ac74d832ad2f23af
IRTIDEATH <enderjhass@gmail.com>
2025-04-17 13:02:06 +0700

    Memperbarui hooks query administrator (#1)

--- hooks/useAdministrator.ts
+++ hooks/useAdministrator.ts
@@ -1,15 +1,34 @@
 "use client"
 
-import { getTotalAbsenKehadiran } from "@/actions/administrator";
-import { useQuery } from "@tanstack/react-query";
+import { getTotalAbsenKehadiran, UpdateTanggal } from "@/actions/administrator";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export function useAdministrator() {
+    const queryClient = useQueryClient();
+
     const absenKehadiran = useQuery({
         queryKey: ["total_absen"],
-        queryFn: getTotalAbsenKehadiran
+        queryFn: getTotalAbsenKehadiran,
+        staleTime: 2 * 60 * 1000
     });
 
+    const updateTanggalKehadiran = useMutation({
+        mutationFn: UpdateTanggal,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["siswas"] });
+            queryClient.invalidateQueries({ queryKey: ["total_absen"] });
+        }
+    })
+
     return {
+        // mutations
+        updateTgl: updateTanggalKehadiran.mutate,
+
+        // Mutation states
+        isUpdating: updateTanggalKehadiran.isPending,
+        isSuccess: updateTanggalKehadiran.isSuccess,
+
+        // Query
         totalAbsen: absenKehadiran.data?.data,
         isLoadingAdministrator: absenKehadiran.isLoading,
         isFetchingAdministrator: absenKehadiran.isFetching,
